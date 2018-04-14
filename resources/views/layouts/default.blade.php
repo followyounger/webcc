222c626bde17789ed474448b43f9609732712e86
vagrant <vagrant@homestead>
2018-04-14 14:50:06 +0000

    finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title','我的第二个练习') --laravel练习</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','帮助')
+@section('content')
+    <h1>我的帮助</h1>
+@stop
